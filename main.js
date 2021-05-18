29f6a423932dddc8c542860faded7214ee560f17
Michael-Fares <mjfares@gmail.com>
2021-05-18 09:47:15 -0500

    finished turn counter to end the game after 10 turns

--- main.js
+++ main.js
@@ -28,18 +28,22 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+//Spec 2 - Generate a hint: generateHint() should take one argument, guess.
 const generateHint = (guess) =>  {
   // your code here
+  //Spec 2.1 - Split up the solution and guess: In generateHint(), create variables solutionArray and guessArray that each split up passed in arguments, [.split](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/splitting on '' (empty string).
 let solutionArray = solution.split('')
 let guessArray = guess.split('')
+
 let correctLetterLocations = 0
 let correctLetters = 0
-
+//Spec 2.2 - Determine correct "letter-locations": Create a variable correctLetterLocations and set it to 0. This variable will record how many correct "letter-locations" were guessed. For instance, a guess of aabc against a solution of deba would yield one correct "letter-location" (b). In a for loop, iterate over the solutionArray, comparing each index of solutionArray against the same index of guessArray. If the item matches, increment correctLetterLocations, and set that index in solutionArray to null.
 for (let i=0; i<solutionArray.length; i++) {
   if (solutionArray[i] === guessArray[i]) {
     correctLetterLocations++
     solutionArray[i] = null
   }
+  //Spec 2.3 - Determine correct "letters": Now that we have nulled the already counted correctLetterLocations, we can see if the guessArray contains any correctLetters that were not in the correct location. Set a variable correctLetters equal to 0, and in a for loop, again iterate over the solutionArray. Using .indexOf, determine if the item at the current index in guessArray appears inside of solutionArray. Save that index in a variable called targetIndex. Now, if targetIndex is greater than -1(it exists in the array), increment correctLetters and set the item in solutionArray at that index equal to null.
   for (let j=0; j<solutionArray.length; j++) {
     let targetIndex = solutionArray.indexOf(guessArray[i])
     if (targetIndex > -1) {
@@ -48,19 +52,28 @@ for (let i=0; i<solutionArray.length; i++) {
     }
   }
 }
+//Spec 2.4 - return hint string: Optionally, you can use the colors package, return a string that prints out the hints you generated, with correctLetterLocations being red, correctLetters being white, and separated by a hyphen. > (NOTE: If you choose to use this color package, only console.log the result. If you return the result your program will fail the tests.)
 return `${correctLetterLocations}-${correctLetters}`
 }
 
 const mastermind = (guess) => {
-  //solution = 'abcd'; // Comment this out to generate a random solution
+  // solution = 'abcd'; 
+  // Comment this out to generate a random solution
   let hint = generateHint(guess)
-
+  
   // your code here
+  //Spec 1 - Detect a correct solution: In mastermind(), if the guess you passed in equals the solution, return 'You guessed it!'; 
   if (guess === solution) {
     return 'You guessed it!'
-  }
+    // Spec 4 - End the game: After 10 incorrect guesses, if the board length equals 10, return 'You ran out of turns! The solution was' and the solution. Otherwise, return 'Guess again.'.
+  } else if (board.length === 10) {
+    console.log(`You ran out of turns! The solution was ${solution}`)
+    return `You ran out of turns! The solution was ${solution}`
+  } else {
+    // Spec 3 - Add guess and hint to the board: Define a variable called hint that collects the returned value of generateHint(guess). .push the guess and the hint (as a combined string) into the board.
 
-  return board.push(guess + hint)
+  return board.push(guess + hint) + 'guess again'
+  }
 }
 
 
