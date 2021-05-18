d1a419dffc664bd17f56fcb72c2f8d8f46a47dbc
Michael-Fares <mjfares@gmail.com>
2021-05-18 09:17:07 -0500

    Mastermind unit tests passed

--- main.js
+++ main.js
@@ -28,13 +28,39 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
+const generateHint = (guess) =>  {
   // your code here
+let solutionArray = solution.split('')
+let guessArray = guess.split('')
+let correctLetterLocations = 0
+let correctLetters = 0
+
+for (let i=0; i<solutionArray.length; i++) {
+  if (solutionArray[i] === guessArray[i]) {
+    correctLetterLocations++
+    solutionArray[i] = null
+  }
+  for (let j=0; j<solutionArray.length; j++) {
+    let targetIndex = solutionArray.indexOf(guessArray[i])
+    if (targetIndex > -1) {
+      correctLetters++
+      solutionArray[targetIndex] = null
+    }
+  }
+}
+return `${correctLetterLocations}-${correctLetters}`
 }
 
 const mastermind = (guess) => {
-  solution = 'abcd'; // Comment this out to generate a random solution
+  //solution = 'abcd'; // Comment this out to generate a random solution
+  let hint = generateHint(guess)
+
   // your code here
+  if (guess === solution) {
+    return 'You guessed it!'
+  }
+
+  return board.push(guess + hint)
 }
 
 
